0b9cb92cfbf439483945226a3f5ceaabfac1f0cf
GermanMoltoni <german.moltoni@gmail.com>
2017-03-29 00:32:40 -0300

    Ejercicio2

---
+++ Clase 2/Ejercicios/Ejercicio2/script.js
@@ -0,0 +1,5 @@
+var arrayMes= new Array("Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiempre","Octubre","Noviembre","Diciembre");
+for(var i=0;i< arrayMes.length;i++)
+{
+  document.write(i+1 +"&nbsp"+ arrayMes[i]+"<BR>");
+}
