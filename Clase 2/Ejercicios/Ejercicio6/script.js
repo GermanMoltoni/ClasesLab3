af1f9b3e122630f55d7c912362cfa4b605e74d12
German Moltoni <german.moltoni@gmail.com>
2017-04-03 14:20:08 -0300

    Ejercicios Clase 2

---
+++ Clase 2/Ejercicios/Ejercicio6/script.js
@@ -0,0 +1,23 @@
+var string = prompt("Ingresar Texto: ").split();
+var mayus = 0;
+var min = 0;
+for(var i=0;i<=string.length;i++)
+{
+    if(string[i] != undefined)
+    {
+        for(var j=0;j<string[i].length;j++)
+        {
+            if(string[i][j] == string[i][j].toUpperCase())
+                mayus=+1;
+            if(string[i][j] == string[i][j].toLowerCase())
+                min+=1;
+        }
+    }
+        
+}
+if(mayus != 0 && min != 0)
+    alert("Hay mayusculas y minusculas");
+else if(mayus != 0 && min ==0)
+    alert("Hay Solo Mayusculas");
+else
+    alert("Hay solo minusculas");
---
+++ Clase 2/Ejercicios/Ejercicio7/script.js
@@ -0,0 +1,4 @@
+var string = prompt();
+var v1 = string.split()[0];
+var v2 = string[2];
+alert(v1);
