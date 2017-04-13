e8fd772bd1f414e9a5d5bc4721792b7ee20efd45
GermanMoltoni <german.moltoni@gmail.com>
2017-04-13 15:25:59 -0300

    Ajax funcionando

--- Clase 4/AJAX/scripts/script.js
+++ Clase 4/AJAX/scripts/script.js
@@ -1,14 +1,16 @@
 
-window.addEventListener('load',inicializarEventos,false);
+/*window.addEventListener('load',inicializarEventos,false);
 // Se crea el evento ya que hay que esperar a que se cargue la pagina para que exista el boton
 function inicializarEventos(){
     var button = document.getElementById('submit');
     button.addEventListener('click',load);
-}
-var req = new XMLHttpRequest();
-function load(){
-    
-    req.open("GET","http://localhost/getPost.php",true);//GET(method),url_de_recurso,true(async)
+}*/
+
+function load()
+{
+    var req = new XMLHttpRequest();
+    req.open("GET","http://germanmoltoni.esy.es/php/mostrar.php",true);//GET(method),url_de_recurso,true(async)
+    //req.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");//POST
     /*
         request type:
                 0 no se envio
@@ -18,12 +20,11 @@ function load(){
                 4 done, respuesta completa en el cliente -> consultar estado en req.readyState
         La info esta en req.responseText
     */
-    req.onreadystatechange = function recibir(){
-        var miDivHTML = document.getElementById('miDivHTML');
-        if(req.readyState == 4)
-            miDivHTML.innerHTML = req.responseText;  
+    req.onreadystatechange = function testState(){
+        if(req.readyState == 4 && req.status == 200)
+            document.getElementById('miDiv').innerHTML = req.responseText;
         else
-            miDivHTML.innerHTML = "Cargando";
-    } //Evento que recibe una funcion que evalua 
-    req.send();
+             document.getElementById('miDiv').innerHTML = "Cargando";
+    }//Evento que recibe una funcion que evalua 
+    req.send(null);
 }
