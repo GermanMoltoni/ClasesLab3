da54013afb926282210a135bb2c8f82f1fa6879e
GermanMoltoni <german.moltoni@gmail.com>
2017-04-25 17:34:36 -0300

    JQuery con formdata

---
+++ Clase5/JQUERY/index.php
@@ -0,0 +1,8 @@
+<?php
+if($_POST && array_key_exists("datos",$_POST))
+{
+    $json  = json_decode($_POST['datos']);
+    echo "Nombre:".$json->nombre." Apellido:".$json->apellido;
+}
+print_r($_POST['nombre']);
+?>
--- Clase5/JQUERY/script.js
+++ Clase5/JQUERY/script.js
@@ -1,16 +1,32 @@
-$(document).ready(
-    function(){
-        $('#dni').val(12344321);//Establece valor al text dni
-        
-        $("#btnSend").click(
-            function(){
-                var nombre = $('#nombre').val();// devuelve el valor del text id
-                var apellido = $('#legajo').val();
-                alert($('myForm').html());
-                $.ajax();
+$(document).ready(function(){
+        $("#btnSend").click(function enviar(){
+        //$('#dni').val(12344321);//Establece valor al text dni
+        //var nombre = $('#nombre').val(); devuelve valor del input por id
+        var formData = new FormData();
+        formData.append("nombre", $('#nombre').val());
+        formData.append("apellido", $('#legajo').val());
+        $.ajax({
+            url:'index.php',
+            type:'POST',
+            //dataType: 'json',
+            cache: false,
+            contentType: false,
+            processData: false,
+            data:formData
+            //data:"datos="+JSON.stringify({'nombre':$('#nombre').val(),'apellido':$('#legajo').val()}),
+            /* success: function(){}// se ejecuta cuando completa correctamente la peticion,
+            error: function(){}// se ejecuta cuando devuelve un error la peticion,
+            complete: function(){}// se ejecuta cuando completa  la peticion*/ 
+            }).done(function(data){
+               $('#myDiv').html(data);
+            }).fail(function(){
+
+            }).always(function(){
+
             });
+    });
+    });
 
 
-    }   
-   
-);
+
+
