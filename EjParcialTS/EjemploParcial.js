c009c322f2d6e4d2a9f9c2a66efd7cbd075e3a22
German Moltoni <german.moltoni@gmail.com>
2017-06-27 14:38:40 -0300

    cuestionario y ejercicio

--- EjParcialTS/EjemploParcial.js
+++ EjParcialTS/EjemploParcial.js
@@ -1,13 +1,10 @@
 "use strict";
-var arr = ["Texto", "Texto", "Texto", "Texto"];
-arr.push("10");
-var array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-var vacio = null;
-var string = '1.2.3.4.5.6';
-var enumeracion;
-(function (enumeracion) {
-    enumeracion[enumeracion["a"] = 0] = "a";
-    enumeracion[enumeracion["b"] = 1] = "b";
-    enumeracion[enumeracion["c"] = 2] = "c";
-    enumeracion[enumeracion["d"] = 3] = "d";
-})(enumeracion || (enumeracion = {}));
+var Usuario = (function () {
+    function Usuario() {
+        this.entrada = 'asdsad';
+    }
+    Usuario.prototype.logear = function () {
+        return true;
+    };
+    return Usuario;
+}());
